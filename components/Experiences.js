b1dbf953135960fe205d4b446f9f054cbc62cc53
coderSomya <isomya13@gmail.com>
2025-02-08 13:44:46 +0530

    add images

--- components/Experiences.js
+++ components/Experiences.js
@@ -7,35 +7,35 @@ export function Experiences() {
         "Worked on core distributed systems, primarily on Rust. Built the entire C++ SDK",
       name: "Weilliptic",
       designation: "Software Engineer - 1",
-      src: "/panora.avif",
+      src: "/stealth.jpeg",
     },
     {
       description:
         "Wrote a lot of Solidity smart contracts. Built a lottery contract. Wrote Base64 encoding decoding functions",
       name: "Lazychain",
       designation: "Software Engineer - 1",
-      src: "/panora.avif",
+      src: "/lazy.jpeg",
     },
     {
         description:
         "Played around with Websockets, Redis, Docker, AWS, TimescaleDB, Nodejs",
       name: "Panora Exchange",
       designation: "Backend and Infrastructure Intern",
-      src: "/panora.avif",
+      src: "/panora.jpeg",
     },
     {
         description:
         "Wrote a lot of SQL and Python and understood various paradigms of fraud-detection. Wrote statistical models to combat them",
       name: "NPCI",
       designation: "ML Engineering Intern",
-      src: "/panora.avif",
+      src: "/npci.png",
     },
     {
         description:
         "Worked on the React frontend and made end to end flow of Time series",
       name: "Dendrite.ai",
       designation: "Software Engineering Intern",
-      src: "/panora.avif",
+      src: "/dendrite.jpeg",
     },
   ];
   return <AnimatedItems items={experiences} />;
